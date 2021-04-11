95ec2bf7ba36ad37e394f3580d2f8899fdd11a7c
stevensc07 <stevensc@uninorte.edu.co>
2021-04-11 10:03:30 -0500

    Json datas

--- src/components/table.js
+++ src/components/table.js
@@ -1,80 +1,12 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Table } from 'antd';
-
-const columns = [
-    {
-        title: 'Nombre',
-        width: 80,
-        dataIndex: 'name',
-        key: 'name',
-        fixed: 'left',
-    },
-    {
-        title: 'Apellido',
-        width: 80,
-        dataIndex: 'lastName',
-        key: 'lastname',
-        fixed: 'left',
-    },
-    {
-        title: 'Identificacion',
-        dataIndex: 'id',
-        key: '1',
-        width: 100,
-    },
-    {
-        title: 'Rol',
-        dataIndex: 'rol',
-        key: '2',
-        width: 80,
-    },
-    {
-        title: 'Estado',
-        dataIndex: 'state',
-        key: '3',
-        width: 80,
-    },
-    {
-        title: 'Telefono',
-        dataIndex: 'tel',
-        key: '4',
-        width: 80,
-    },
-    {
-        title: 'Correo electronico',
-        dataIndex: 'mail',
-        key: '5',
-        width: 150,
-    },
-
-    {
-        title: 'Action',
-        key: 'operation',
-        fixed: 'right',
-        width: 80,
-        render: () => <a>action</a>,
-    },
-];
-
-const data = [];
-for (let i = 0; i < 100; i++) {
-    data.push({
-        key: i,
-        name: `Stevens ${i}`,
-        lastName: `Correa ${i}`,
-        id: 1045747348 + i,
-        rol: `Administrador ${i}`,
-        state: `Activo ${i}`,
-        tel: 3215362088 + i,
-        mail: `stevensc@uninorte.edu.co ${i}`,
-    });
-}
+import  * as Data from '../controller/tableController'
 
 export default function DataTable() {
     return (
         <div style={{ height: 400, width: '100%', paddingLeft:'1%', paddingRight:'1%', marginBottom:'10%' }}>
-            <Table style={{ paddingTop: 20 }} tableLayout={'fixed'} size={'small'} columns={columns} dataSource={data} scroll={{ x: 1500, y: 300 }} />
+            <Table style={{ paddingTop: 20 }} tableLayout={'fixed'} size={'small'} columns={Data.Columns} dataSource={Data.data} scroll={{ x: 1500, y: 300 }} />
         </div>
     );
 }
---
+++ src/views/private/home/dashboard/makeStyles.js
@@ -0,0 +1,65 @@
+export const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      backgroundColor: '#449df2'
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      backgroundColor: '#fff'
+    },
+    menuButton: {
+      color: 'blue'
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      background: 'linear-gradient(0deg, rgba(61,25,183,1) 0%, rgba(96,146,228,1) 42%, rgba(96,146,228,1) 65%, rgba(68,157,242,1) 100%)',
+    },
+    drawerClose: {
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: 'hidden',
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(9) + 1,
+      },
+      background: 'linear-gradient(0deg, rgba(61,25,183,1) 0%, rgba(96,146,228,1) 42%, rgba(96,146,228,1) 65%, rgba(68,157,242,1) 100%)',
+    },
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }));

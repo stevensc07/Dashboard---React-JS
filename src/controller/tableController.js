1f3c5ae7cc88e2bf69bd0bf04aa35e03d263aa30
stevensc07 <stevensc@uninorte.edu.co>
2021-04-11 10:03:41 -0500

    Json datas

--- src/controller/tableController.js
+++ src/controller/tableController.js
@@ -1,18 +1,3 @@
-/* const dataSource = [
-    {
-        key: '1',
-        name: 'Mike',
-        age: 32,
-        address: '10 Downing Street',
-    },
-    {
-        key: '2',
-        name: 'John',
-        age: 42,
-        address: '10 Downing Street',
-    },
-]; */
-
 export const Columns = [
   {
     title: 'Nombre',
